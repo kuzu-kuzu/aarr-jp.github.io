86dcf16f5ad0ddc2d7a8616ec58daf02a728806e
aarr-jp <79005908+aarr-jp@users.noreply.github.com>
2022-10-12 00:04:54 +0900

    add: imgur showcase

---
+++ src/components/ImgurImageShowcase.tsx
@@ -0,0 +1,99 @@
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardContent from '@mui/material/CardContent';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import { type FC, useState } from 'react';
+import CommonDialog from '~/components/CommonDialog';
+
+type ImgurImageShowcaseItem = Readonly<{
+  ext: '.gif' | '.png',
+  imgurId: string,
+  message: string,
+  deviceWarning?: boolean
+}>;
+
+type ImgurImageShowcaseProps = Readonly<{
+  images: readonly ImgurImageShowcaseItem[],
+  title: string
+}>;
+
+const openImage = ({ imgurId, ext }: ImgurImageShowcaseItem): void => {
+  const a = document.createElement('a');
+
+  a.href = `https://i.imgur.com/${imgurId}${ext}`;
+  a.target = '_blank';
+  a.rel = 'noopener noreferrer';
+
+  a.click();
+};
+
+const ImgurImageShowcase: FC<ImgurImageShowcaseProps> = ({ images, title }) => {
+  const [image, setImage] = useState<ImgurImageShowcaseItem>();
+
+  return (
+    <Box>
+      <CommonDialog
+        open={typeof image !== 'undefined'}
+        message='この画像は環境によっては重くなる可能性があります'
+        confirmButtonText='開きます'
+        onCancel={() => {
+          setImage(undefined);
+        }}
+        onConfirm={() => {
+          setImage(undefined);
+
+          if (image) {
+            openImage(image);
+          }
+        }}
+      />
+      <Stack spacing={1}>
+        <Typography>{title}</Typography>
+        <Stack
+          overflow='auto'
+          direction='row'
+          spacing={2}
+        >
+          {images.map(image => {
+            const { message, imgurId, ext, deviceWarning } = image;
+
+            return (
+              <Card
+                key={`${imgurId}${ext}`}
+                sx={{ flexShrink: 0 }}
+              >
+                <CardContent
+                  sx={{
+                    '&:last-child': {
+                      paddingBottom: '0.5rem'
+                    }
+                  }}
+                >
+                  <Stack spacing={1}>
+                    <CardActionArea onClick={() => deviceWarning ? setImage(image) : openImage(image)}>
+                      <Box
+                        sx={{ verticalAlign: 'middle' }}
+                        crossOrigin='anonymous'
+                        component='img'
+                        src={`https://i.imgur.com/${imgurId}b${ext}`}
+                      />
+                    </CardActionArea>
+                    <Typography color='text.secondary'>{message}</Typography>
+                  </Stack>
+                </CardContent>
+              </Card>
+            );
+          })}
+        </Stack>
+      </Stack>
+    </Box>
+  );
+};
+
+export default ImgurImageShowcase;
+export {
+  type ImgurImageShowcaseItem,
+  type ImgurImageShowcaseProps
+};
--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -1,104 +1,51 @@
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActionArea from '@mui/material/CardActionArea';
-import CardContent from '@mui/material/CardContent';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
-import { type FC, useState } from 'react';
-import CommonDialog from '~/components/CommonDialog';
+import { type FC } from 'react';
+import ImgurImageShowcase from '~/components/ImgurImageShowcase';
 import Seo from '~/components/Seo';
 
-type GalleryImage = Readonly<{
-  ext: `.${string}`,
-  imgurId: string,
-  summary: string,
-  phoneWarning?: boolean
-}>;
+const GalleryPage: FC = () => (
+  <>
+    <Seo
+      title='ギャラリー'
+      description='コミュニティの画像などを置いています。'
+    />
 
-const images: readonly GalleryImage[] = [
-  {
-    ext: '.png',
-    imgurId: 'Uprp9eZ',
-    summary: 'DiscordのAARRのサーバーアイコン（2022年10月9日～）'
-  },
-  {
-    ext: '.gif',
-    imgurId: '45f4YDN',
-    phoneWarning: true,
-    summary: 'DiscordのAARRのサーバーアイコン（2021年12月4日～）'
-  },
-  {
-    ext: '.gif',
-    imgurId: 'k9FnwHw',
-    phoneWarning: true,
-    summary: 'DiscordのAARRのサーバーバナー（2021年12月5日～）'
-  }
-];
-
-const openImage = ({ imgurId, ext }: GalleryImage): void => {
-  const a = document.createElement('a');
-
-  a.href = `https://i.imgur.com/${imgurId}${ext}`;
-  a.target = '_blank';
-  a.rel = 'noopener noreferrer';
-
-  a.click();
-};
-
-const GalleryPage: FC = () => {
-  const [image, setImage] = useState<GalleryImage>();
-
-  return (
-    <>
-      <Seo
-        title='ギャラリー'
-        description='コミュニティの画像などを置いています。'
-      />
-      <CommonDialog
-        open={typeof image !== 'undefined'}
-        message='この画像は環境によっては重くなる可能性があります'
-        confirmButtonText='開きます'
-        onCancel={() => {
-          setImage(undefined);
-        }}
-        onConfirm={() => {
-          setImage(undefined);
-
-          if (image) {
-            openImage(image);
-          }
-        }}
-      />
-      <Stack spacing={2} paddingBottom='2rem'>
-        <Typography fontSize='small' color='text.secondary'>
-          画像をクリック（またはタップ）でオリジナルサイズの画像を開く
-        </Typography>
-        {images.map(image => {
-          const { imgurId, summary, ext, phoneWarning = false } = image;
-
-          return (
-            <Card key={`${imgurId}${ext}`}>
-              <CardContent>
-                <Stack alignItems='flex-start' spacing={1}>
-                  <Typography>{summary}</Typography>
-                  <Box>
-                    <CardActionArea onClick={() => phoneWarning ? setImage(image) : openImage(image)}>
-                      <Box
-                        sx={{ verticalAlign: 'middle' }}
-                        component='img'
-                        crossOrigin='anonymous'
-                        src={`https://i.imgur.com/${imgurId}b${ext}`}
-                      />
-                    </CardActionArea>
-                  </Box>
-                </Stack>
-              </CardContent>
-            </Card>
-          );
-        })}
+    <Stack spacing={2} paddingBottom='2rem'>
+      <Typography fontSize='small' color='text.secondary'>
+        画像をクリック（またはタップ）でオリジナルサイズの画像を開く
+      </Typography>
+      <Stack spacing={4}>
+        <ImgurImageShowcase
+          title='AARRのDiscordサーバーアイコン'
+          images={[
+            {
+              ext: '.png',
+              imgurId: 'Uprp9eZ',
+              message: '2022年10月9日～'
+            },
+            {
+              deviceWarning: true,
+              ext: '.gif',
+              imgurId: '45f4YDN',
+              message: '2021年12月4日～'
+            }
+          ]}
+        />
+        <ImgurImageShowcase
+          title='AARRのDiscordサーバーバナー'
+          images={[
+            {
+              deviceWarning: true,
+              ext: '.gif',
+              imgurId: 'k9FnwHw',
+              message: '2021年12月5日～'
+            }
+          ]}
+        />
       </Stack>
-    </>
-  );
-};
+    </Stack>
+  </>
+);
 
 export default GalleryPage;
